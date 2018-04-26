25fdafeacbd02301655af7aab79c90fdc113adc9
slava <s.v.savenko@gmail.com>
2018-04-26 23:00:38 +0200

    work on Kernel class interface

--- src/include/vuh/kernel.hpp
+++ src/include/vuh/kernel.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "device.h"
 #include "string_view.h"
 
 #include <vulkan/vulkan.hpp>
@@ -10,20 +11,23 @@ namespace vuh {
 	///
 	template<class... Ts> struct arrays_of{};
 
+	template<class Specs, class Params, class Arrays> class Program;
+
 	/// Brings together shader interfaces declaration and the spirv code.
 	/// Needs to be linked to vuh::Device to get the runnable program.
-	template<class specs_t    ///< tuple of specialization parameters
-	         , class arrays_t ///< arrays_of<> structure of array parameters to shader
-	         , class params_t ///< shader push parameters interface
-	         >
+	template< class Specs   ///< tuple of specialization parameters
+	        , class Params  ///< shader push parameters interface
+	        , class Arrays  ///< arrays_of<> structure of array parameters to shader
+	        >
 	class Kernel {
 	public:
-		using specs_t = specs_t;
-		using array_t = arrays_t;
-		using params_t = params_t;
+		using specs_t = Specs;
+		using array_t = Arrays;
+		using params_t = Params;
 
 		explicit Kernel(string_view path, vk::ShaderModuleCreateFlags flags={});
-
+		auto on(vuh::Device& device)-> Program<specs_t, array_t, params_t>;
+		auto on(vuh::Device& device, uint32_t queue_id)-> Program<specs_t, array_t, params_t>;
 	protected: // data
 		std::vector<char> _code;            ///< binary blob with a spirv shader code
 		vk::ShaderModuleCreateFlags _flags; ///< shader create flags
