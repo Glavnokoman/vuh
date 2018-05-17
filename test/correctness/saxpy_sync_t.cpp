b9e943d572dcf14ba2eda6f492456ee5fff6cb26
slava <s.v.savenko@gmail.com>
2018-05-17 20:52:07 +0200

    add test for rebindinding parameters

--- test/correctness/saxpy_sync_t.cpp
+++ test/correctness/saxpy_sync_t.cpp
@@ -67,6 +67,38 @@ TEST_CASE("saxpy_repeated", "[correctness]"){
 	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
 }
 
+TEST_CASE("saxpy_rebound", "[correctness]"){
+	auto y = std::vector<float>(128, 1.0f);
+	auto x = std::vector<float>(128, 2.0f);
+	const auto a = 0.1f; // saxpy scaling constant
+	const auto n_repeat = 10;
+
+	auto out_ref = y;
+	for(size_t i = 0; i < y.size(); ++i){
+		out_ref[i] += n_repeat*a*x[i];
+	}
+
+	auto instance = vuh::Instance();
+	auto device = instance.devices().at(0);            // just get the first compute-capable device
+
+	auto d_y = vuh::Array<float>::fromHost(device, y); // allocate memory on device and copy data from host
+	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
+
+	using Specs = vuh::typelist<uint32_t>;
+	struct Params{uint32_t size; float a;};
+
+	auto program = vuh::Program<Specs, Params>(device, "../shaders/saxpy.spv");
+	program.grid(128/64)                            // set number of wrokgroups to run
+			 .spec(64);                               // set the specialization constants
+	for(size_t i = 0; i < n_repeat; ++i){
+		program({128, a}, d_y, d_x);
+	}
+	d_y.toHost(y);                                     // copy data back to host
+
+	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
+}
+
+
 TEST_CASE("saxpy_nospec", "[correctness]"){
 	auto y = std::vector<float>(128, 1.0f);
 	auto x = std::vector<float>(128, 2.0f);
@@ -108,7 +140,7 @@ TEST_CASE("saxpy_nopush", "[correctness]"){
 	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
 
 	using Specs = vuh::typelist<uint32_t>;
-	auto program = vuh::Program<Specs, vuh::typelist<>>(device, "../shaders/saxpy_nopush.spv"); // define the kernel by linking interface and spir-v implementation
+	auto program = vuh::Program<Specs>(device, "../shaders/saxpy_nopush.spv"); // define the kernel by linking interface and spir-v implementation
 	program.grid(2).spec(64)(d_y, d_x);              // run once, wait for completion
 	d_y.toHost(y);                                     // copy data back to host
 
