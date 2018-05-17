53a5f3d0f076ac3f99983e504d6c75f77fb498a9
slava <s.v.savenko@gmail.com>
2018-05-17 22:30:01 +0200

    refactor tests to use sections

--- test/correctness/saxpy_sync_t.cpp
+++ test/correctness/saxpy_sync_t.cpp
@@ -10,7 +10,7 @@
 
 using test::approx;
 
-TEST_CASE("saxpy_once", "[correctness]"){
+TEST_CASE("saxpy_once_1D", "[correctness]"){
 	auto y = std::vector<float>(128, 1.0f);
 	auto x = std::vector<float>(128, 2.0f);
 	const auto a = 0.1f; // saxpy scaling constant
@@ -26,87 +26,41 @@ TEST_CASE("saxpy_once", "[correctness]"){
 	auto d_y = vuh::Array<float>::fromHost(device, y); // allocate memory on device and copy data from host
 	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
 
-	using Specs = vuh::typelist<uint32_t>;
-	struct Params{uint32_t size; float a;};
-	auto program = vuh::Program<Specs, Params>(device, "../shaders/saxpy.spv"); // define the kernel by linking interface and spir-v implementation
-	program.grid(128/64).spec(64)({128, a}, d_y, d_x); // run once, wait for completion
-	d_y.toHost(y);                                     // copy data back to host
+	SECTION("nonempty specialization and push constants"){
+		using Specs = vuh::typelist<uint32_t>;
+		struct Params{uint32_t size; float a;};
+		auto program = vuh::Program<Specs, Params>(device, "../shaders/saxpy.spv"); // define the kernel by linking interface and spir-v implementation
+		program.grid(128/64).spec(64)({128, a}, d_y, d_x); // run once, wait for completion
+		d_y.toHost(y);                                     // copy data back to host
 
-	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
-}
-
-TEST_CASE("saxpy_nospec", "[correctness]"){
-	auto y = std::vector<float>(128, 1.0f);
-	auto x = std::vector<float>(128, 2.0f);
-	const auto a = 0.1f; // saxpy scaling constant
-
-	auto out_ref = y;
-	for(size_t i = 0; i < y.size(); ++i){
-		out_ref[i] += a*x[i];
+		REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
 	}
+	SECTION("no specialization constants"){
+		struct Params{uint32_t size; float a;};
+		auto program = vuh::Program<vuh::typelist<>, Params>(device, "../shaders/saxpy_nospec.spv");
+		program.grid(2)({128, a}, d_y, d_x);
+		d_y.toHost(y);
 
-	auto instance = vuh::Instance();
-	auto device = instance.devices().at(0);            // just get the first compute-capable device
-
-	auto d_y = vuh::Array<float>::fromHost(device, y); // allocate memory on device and copy data from host
-	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
-
-	struct Params{uint32_t size; float a;};
-	auto program = vuh::Program<vuh::typelist<>, Params>(device, "../shaders/saxpy_nospec.spv"); // define the kernel by linking interface and spir-v implementation
-	program.grid(2)({128, a}, d_y, d_x);              // run once, wait for completion
-	d_y.toHost(y);                                     // copy data back to host
-
-	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
-}
-
-TEST_CASE("saxpy_nopush", "[correctness]"){
-	auto y = std::vector<float>(128, 1.0f);
-	auto x = std::vector<float>(128, 2.0f);
-	const auto a = 0.1f; // saxpy scaling constant
-
-	auto out_ref = y;
-	for(size_t i = 0; i < y.size(); ++i){
-		out_ref[i] += a*x[i];
+		REQUIRE(y == approx(out_ref).eps(1.e-5));
 	}
+	SECTION("no push constants"){
+		using Specs = vuh::typelist<uint32_t>;
+		auto program = vuh::Program<Specs>(device, "../shaders/saxpy_nopush.spv");
+		program.grid(2).spec(64)(d_y, d_x);
+		d_y.toHost(y);
 
-	auto instance = vuh::Instance();
-	auto device = instance.devices().at(0);            // just get the first compute-capable device
-
-	auto d_y = vuh::Array<float>::fromHost(device, y); // allocate memory on device and copy data from host
-	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
-
-	using Specs = vuh::typelist<uint32_t>;
-	auto program = vuh::Program<Specs>(device, "../shaders/saxpy_nopush.spv"); // define the kernel by linking interface and spir-v implementation
-	program.grid(2).spec(64)(d_y, d_x);              // run once, wait for completion
-	d_y.toHost(y);                                     // copy data back to host
-
-	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
-}
-
-TEST_CASE("saxpy_nothing", "[correctness]"){
-	auto y = std::vector<float>(128, 1.0f);
-	auto x = std::vector<float>(128, 2.0f);
-	const auto a = 0.1f; // saxpy scaling constant
-
-	auto out_ref = y;
-	for(size_t i = 0; i < y.size(); ++i){
-		out_ref[i] += a*x[i];
+		REQUIRE(y == approx(out_ref).eps(1.e-5));
 	}
+	SECTION("no push or specialization constants"){
+		auto program = vuh::Program<>(device, "../shaders/saxpy_noth.spv");
+		program.grid(2)(d_y, d_x);
+		d_y.toHost(y);
 
-	auto instance = vuh::Instance();
-	auto device = instance.devices().at(0);            // just get the first compute-capable device
-
-	auto d_y = vuh::Array<float>::fromHost(device, y); // allocate memory on device and copy data from host
-	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
-
-	auto program = vuh::Program<>(device, "../shaders/saxpy_noth.spv"); // define the kernel by linking interface and spir-v implementation
-	program.grid(2)(d_y, d_x);              // run once, wait for completion
-	d_y.toHost(y);                                     // copy data back to host
-
-	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
+		REQUIRE(y == approx(out_ref).eps(1.e-5));
+	}
 }
 
-TEST_CASE("saxpy_repeated", "[correctness]"){
+TEST_CASE("saxpy_repeated_1D", "[correctness]"){
 	auto y = std::vector<float>(128, 1.0f);
 	auto x = std::vector<float>(128, 2.0f);
 	const auto a = 0.1f; // saxpy scaling constant
@@ -118,53 +72,35 @@ TEST_CASE("saxpy_repeated", "[correctness]"){
 	}
 
 	auto instance = vuh::Instance();
-	auto device = instance.devices().at(0);            // just get the first compute-capable device
+	auto device = instance.devices().at(0);
 
-	auto d_y = vuh::Array<float>::fromHost(device, y); // allocate memory on device and copy data from host
-	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
+	auto d_y = vuh::Array<float>::fromHost(device, y);
+	auto d_x = vuh::Array<float>::fromHost(device, x);
 
-	using Specs = vuh::typelist<uint32_t>;
-	struct Params{uint32_t size; float a;};
+	SECTION("bind once run multiple"){
+		using Specs = vuh::typelist<uint32_t>;
+		struct Params{uint32_t size; float a;};
+		auto program = vuh::Program<Specs, Params>(device, "../shaders/saxpy.spv");
+		program.grid(128/64)                            // set number of wrokgroups to run
+				 .spec(64)                                // set the specialization constants
+				 .bind({128, a}, d_y, d_x);	            // bind arrays and non-array parameters
+		for(size_t i = 0; i < n_repeat; ++i){
+			program.run();
+		}
+		d_y.toHost(y);
 
-	auto program = vuh::Program<Specs, Params>(device, "../shaders/saxpy.spv");
-	program.grid(128/64)                            // set number of wrokgroups to run
-			 .spec(64)                                // set the specialization constants
-			 .bind({128, a}, d_y, d_x);	            // bind arrays and non-array parameters
-	for(size_t i = 0; i < n_repeat; ++i){
-		program.run();
+		REQUIRE(y == approx(out_ref).eps(1.e-5));
 	}
-	d_y.toHost(y);                                     // copy data back to host
+	SECTION("multiple bind and run"){
+		using Specs = vuh::typelist<uint32_t>;
+		struct Params{uint32_t size; float a;};
+		auto program = vuh::Program<Specs, Params>(device, "../shaders/saxpy.spv");
+		program.grid(128/64).spec(64);
+		for(size_t i = 0; i < n_repeat; ++i){
+			program({128, a}, d_y, d_x);
+		}
+		d_y.toHost(y);
 
-	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
-}
-
-TEST_CASE("saxpy_rebound", "[correctness]"){
-	auto y = std::vector<float>(128, 1.0f);
-	auto x = std::vector<float>(128, 2.0f);
-	const auto a = 0.1f; // saxpy scaling constant
-	const auto n_repeat = 10;
-
-	auto out_ref = y;
-	for(size_t i = 0; i < y.size(); ++i){
-		out_ref[i] += n_repeat*a*x[i];
+		REQUIRE(y == approx(out_ref).eps(1.e-5));
 	}
-
-	auto instance = vuh::Instance();
-	auto device = instance.devices().at(0);            // just get the first compute-capable device
-
-	auto d_y = vuh::Array<float>::fromHost(device, y); // allocate memory on device and copy data from host
-	auto d_x = vuh::Array<float>::fromHost(device, x); // same for x
-
-	using Specs = vuh::typelist<uint32_t>;
-	struct Params{uint32_t size; float a;};
-
-	auto program = vuh::Program<Specs, Params>(device, "../shaders/saxpy.spv");
-	program.grid(128/64)                            // set number of wrokgroups to run
-			 .spec(64);                               // set the specialization constants
-	for(size_t i = 0; i < n_repeat; ++i){
-		program({128, a}, d_y, d_x);
-	}
-	d_y.toHost(y);                                     // copy data back to host
-
-	REQUIRE(y == approx(out_ref).eps(1.e-5).verbose());
 }
