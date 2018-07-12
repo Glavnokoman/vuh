8473ad442f422038c4705f6a72c557c02168c453
slava <s.v.savenko@gmail.com>
2018-07-12 23:26:48 +0200

    fill in some test code

--- test/correctness/array_async_t.cpp
+++ test/correctness/array_async_t.cpp
@@ -12,11 +12,6 @@ using std::end;
 TEST_CASE("async copy device-local memory", "[array][correctness][async]"){
 	constexpr auto arr_size = size_t(128);
 	const auto host_data = std::vector<float>(arr_size, 3.14f);
-//	const auto host_data_doubled = [&]{
-//		auto r = host_data;
-//		for(auto& x: r){ x *= 2.f; }
-//		return r;
-//	}();
 
 	auto instance = vuh::Instance();
 	auto device = instance.devices().at(0);
@@ -28,52 +23,51 @@ TEST_CASE("async copy device-local memory", "[array][correctness][async]"){
 			fence.wait();
 			REQUIRE(array.toHost<std::vector<float>>() == host_data);
 		}
+		SECTION("async copy from host. scoped"){
+			auto array = vuh::Array<float, vuh::mem::Device>(device, arr_size);
+			{
+				auto fence = vuh::copy_async(begin(host_data), end(host_data), begin(array));
+			}
+			REQUIRE(array.toHost<std::vector<float>>() == host_data);
+		}
+		SECTION("async copy to host. 2 halves"){
+			auto array = vuh::Array<float, vuh::mem::Device>(device, arr_size);
+			{
+				auto f1 = vuh::copy_async(begin(host_data), begin(host_data) + arr_size/2
+				                          , begin(array));
+				auto f2 = vuh::copy_async(begin(host_data) + arr_size/2, end(host_data)
+				                          , begin(array) + arr_size/2);
+			}
+			REQUIRE(array.toHost<std::vector<float>>() == host_data);
+		}
 		SECTION("data transfer to host"){
+			auto array = vuh::Array<float, vuh::mem::Device>(device, host_data);
+			auto host_data_tst = std::vector<float>(arr_size, 0.f);
+			auto fence = vuh::copy_async(begin(array), end(array), begin(host_data_tst));
+			fence.wait();
+			REQUIRE(host_data_tst == host_data);
 		}
 		SECTION("async copy to host. scoped"){
+			auto array = vuh::Array<float, vuh::mem::Device>(device, host_data);
+			auto host_data_tst = std::vector<float>(arr_size, 0.f);
+			{
+				auto fence = vuh::copy_async(begin(array), end(array), begin(host_data_tst));
+			}
+			REQUIRE(host_data_tst == host_data);
 		}
-		SECTION("async copy from host. scoped"){
-		}
-		SECTION("async copy to host. iterators"){
-		}
-		SECTION("async copy from host. iterators"){
+		SECTION("async copy from host. 2 halves"){
+			auto array = vuh::Array<float, vuh::mem::Device>(device, host_data);
+			auto host_data_tst = std::vector<float>(arr_size, 0.f);
+			{
+				auto f1 = vuh::copy_async(begin(array), begin(array) + arr_size/2
+				                          , begin(host_data_tst));
+				auto f2 = vuh::copy_async(begin(array) + arr_size/2, end(array)
+				                          , begin(host_data_tst) + arr_size/2);
+			}
+			REQUIRE(host_data_tst == host_data);
 		}
 	}
 //	SECTION("device-local host-visible (unified) memory"){
-//		try{
-//			auto d1 = vuh::Array<float, vuh::mem::Unified>(device, arr_size);
-//		} catch (...){
-//			std::cerr << "skipping unified memory array tests..." "\n";
-//			return;
-//		}
-
-//		SECTION("size constructor"){
-//			auto array = vuh::Array<float, vuh::mem::Unified>(device, arr_size);
-//			REQUIRE(array.size() == arr_size);
-//			REQUIRE(array.size_bytes() == arr_size*sizeof(float));
-//		}
-//		SECTION("size constructor with value initialization"){
-//			auto array = vuh::Array<float, vuh::mem::Unified>(device, arr_size, 3.14f);
-//			REQUIRE(array.size() == arr_size);
-//			REQUIRE(std::vector<float>(begin(array), end(array)) == host_data);
-//		}
-//		SECTION("construct from range"){
-//			auto array = vuh::Array<float, vuh::mem::Unified>(device, begin(host_data), end(host_data));
-//			REQUIRE(std::vector<float>(begin(array), end(array)) == host_data);
-//		}
-//		SECTION("random access with operator []"){
-//			auto array = vuh::Array<float, vuh::mem::Unified>(device, arr_size, 3.14f);
-//			REQUIRE(array[arr_size/2] == Approx(3.14f));
-//			array[arr_size/2] = 2.71f;
-//			REQUIRE(array[arr_size/2] == Approx(2.71f));
-//		}
-//		SECTION("begin() and end() iterators"){
-//			auto array = vuh::Array<float, vuh::mem::Unified>(device, arr_size, 3.14f);
-//			REQUIRE(std::vector<float>(begin(array), end(array)) == host_data);
-//			for(auto& x: array){
-//				x *= 2.f;
-//			}
-//			REQUIRE(std::vector<float>(begin(array), end(array)) == host_data_doubled);
 //		}
 //	}
 }
