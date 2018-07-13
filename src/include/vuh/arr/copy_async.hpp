584ccfc97eaba4e0e0f5edceee2409912b2a4dd9
slava <s.v.savenko@gmail.com>
2018-07-13 22:39:01 +0200

    specialize copy_async() template a bit

--- src/include/vuh/arr/copy_async.hpp
+++ src/include/vuh/arr/copy_async.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <vuh/program.hpp>
+#include "arrayIter.hpp"
 
 namespace vuh {
 
-	/// copy data
-	template<class Begin, class End, class ArrayIter>
-	auto copy_async(Begin begin, End end, ArrayIter array_iter)-> vuh::Fence {
+	/// async copy data from host to device
+	template<class Begin, class End, class Array>
+	auto copy_async(Begin src_begin, End src_end, vuh::ArrayIter<Array> dst_begin)-> vuh::Fence {
+		throw "not implemented";
+	}
+
+	/// async copy data from device to host
+	template<class Array, class Begin>
+	auto copy_async(ArrayIter<Array> src_begin, ArrayIter<Array> src_end, Begin dst_begin)-> vuh::Fence {
 		throw "not implemented";
 	}
 } // namespace vuh
