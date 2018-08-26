0b63d80206e2590056e758e5c37be8657954c656
slava <s.v.savenko@gmail.com>
2018-08-26 16:39:39 +0200

    bump dependencies versions

--- config/install_dependencies.sh
+++ config/install_dependencies.sh
@@ -6,15 +6,13 @@ cget init --std=c++14
 
 # cget install KhronosGroup/Vulkan-Headers
 
-# https://github.com/Glavnokoman/Vulkan-Loader.git
-git clone --branch sdk-1.1.77.0 --depth 1 --recurse-submodules https://github.com/KhronosGroup/Vulkan-Headers.git ${CGET_PREFIX}/src/vulkan-headers
+git clone --branch sdk-1.1.82.0 --depth 1 --recurse-submodules https://github.com/KhronosGroup/Vulkan-Headers.git ${CGET_PREFIX}/src/vulkan-headers
 cget install ${CGET_PREFIX}/src/vulkan-headers
 
-#git clone --recurse-submodules -b fix_find_vulkan_headers https://github.com/Glavnokoman/Vulkan-Loader.git ${CGET_PREFIX}/src/vulkan-loader
-git clone --branch sdk-1.1.77.0 --depth 1 --recurse-submodules https://github.com/KhronosGroup/Vulkan-Loader ${CGET_PREFIX}/src/vulkan-loader
+git clone --branch sdk-1.1.82.0 --depth 1 --recurse-submodules https://github.com/KhronosGroup/Vulkan-Loader ${CGET_PREFIX}/src/vulkan-loader
 cget install ${CGET_PREFIX}/src/vulkan-loader -DBUILD_WSI_WAYLAND_SUPPORT=OFF -DBUILD_TESTS=OFF
 
-git clone --branch 6.2.2596 --depth 1 --recurse-submodules https://github.com/KhronosGroup/glslang.git ${CGET_PREFIX}/src/glslang
+git clone --branch 7.8.2853 --depth 1 --recurse-submodules https://github.com/KhronosGroup/glslang.git ${CGET_PREFIX}/src/glslang
 cget install ${CGET_PREFIX}/src/glslang -DENABLE_HLSL=OFF -DENABLE_AMD_EXTENSIONS=OFF -DENABLE_NV_EXTENSIONS=OFF -DENABLE_OPT=OFF
 
 cget install catchorg/Catch2@v2.3.0 -DBUILD_TESTING=OFF
