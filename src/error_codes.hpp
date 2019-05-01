8a789852fd506ac2b49c78b70cef3a1d4ebe12c7
slava <s.v.savenko@gmail.com>
2019-05-01 21:50:58 +0200

    added vuh specific errors

--- src/error_codes.hpp
+++ src/error_codes.hpp
@@ -3,15 +3,21 @@
 #include <vulkan/vulkan_core.h>
 
 namespace vuh::error {
-/// messages for vulkan and vuh error codes
-inline auto text(VkResult)-> const char* {
-	return "TBD";
-}
-
 /// vuh specific error codes
 enum VuhError: int32_t {
 	  NoComputeCapableQueueFamilySpecified = VK_RESULT_END_RANGE + 1
 	, NoTransferCapableQueueFamilySpecified
 };
+
+/// messages for vulkan and vuh error codes
+inline auto text(VkResult)-> const char* {
+	return "TBD";
+}
+
+/// messages for vuh specific errors
+inline auto text(VuhError)-> const char* {
+	return "TBD";
+}
+
 
 } // namespace vuh::error
--- src/error_except.hpp
+++ src/error_except.hpp
@@ -15,6 +15,12 @@ public:
 	explicit Error(VkResult err): std::runtime_error(error::text(err)){}
 }; //
 
+class ErrorVuh: std::runtime_error {
+public:
+	using std::runtime_error::runtime_error;
+	explicit ErrorVuh(error::VuhError err): std::runtime_error(error::text(err)) {}
+};
+
 ///
 inline auto VUH_CHECK(VkResult err) {
 	if(err != VK_SUCCESS){
@@ -37,9 +43,14 @@ inline auto VUH_CHECKOUT(){}
 template<class T>
 inline auto VUH_CHECKOUT_RET(T){}
 
-/// signal vuh specific error
-inline auto VUH_SIGNAL(std::int32_t err) {
+/// signal vulkan error
+[[noreturn]] inline auto VUH_SIGNAL(VkResult err) {
+	throw Error(err);
+}
 
+/// signal vuh specific error
+[[noreturn]] inline auto VUH_SIGNAL(error::VuhError err) {
+	throw ErrorVuh(err);
 }
 
 } // namespace vuh
