898b3f47efd99e41cfc82cde6523ed48604c2e45
slava <s.v.savenko@gmail.com>
2019-05-11 14:28:52 +0200

    buffers test is OK

--- src/include/vuh/algorithm.hpp
+++ src/include/vuh/algorithm.hpp
@@ -15,59 +15,89 @@ namespace vuh {
 /// Synchronous copy of the data from the buffer to host iterable
 /// using the default transfer queue of the device to which the buffer belongs.
 /// @todo add traits check for Buffer template parameter
-template<class BufferView_t, class OutputIt>
-auto copy(const BufferView_t& view, OutputIt dst)-> OutputIt{
-	if(view.host_visible()){
-		auto data = view.host_data();
-		std::copy(std::begin(data), std::end(data), dst);
+template<class DeviceData_t, class OutputIt>
+auto copy(const DeviceData_t& ddata, OutputIt dst)-> OutputIt {
+	if(ddata.host_visible()){
+		auto hd = ddata.host_data();
+		std::copy(std::begin(hd), std::end(hd), dst);
 	} else {
-		using Stage = BufferHost< typename BufferView_t::value_type
+		using Stage = BufferHost< typename DeviceData_t::value_type
 		                        , AllocatorDevice<allocator::traits::HostCached>>;
-		auto stage = Stage(view.device(), view.size());
-		view.device().default_transfer().copy_sync(view, stage);
+		auto stage = Stage(ddata.device(), ddata.size());
+		VUH_CHECKOUT_RET(dst);
+		ddata.device().default_transfer().copy_sync(ddata, stage);
+		VUH_CHECKOUT_RET(dst);
 		std::copy(std::begin(stage), std::end(stage), dst);
 	}
-	return std::next(dst, view.size());
+	return std::next(dst, ddata.size());
 }
 
-///
-template<class BufferView_t, class InputIt>
-auto copy(InputIt first, InputIt last, BufferView_t& buf)-> InputIt {
+/// Synchronous copy of the data from host range to device buffer
+/// using the default transfer queue of the device on which the buffer was allocated.
+/// Buffer is supposed to have sufficient memory to accomodate that data.
+template<class DeviceData_t, class InputIt>
+auto copy(InputIt first, InputIt last, DeviceData_t& buf)-> void {
+	assert(std::distance(first, last) <= buf.size());
 	if(buf.host_visible()){
 		auto data = buf.host_data();
 		std::copy(first, last, data.begin());
 	} else {
-		using Stage = BufferHost<typename BufferView_t::value_type
+		using Stage = BufferHost<typename DeviceData_t::value_type
 		                        , AllocatorDevice<allocator::traits::HostCoherent>>;
 		auto stage = Stage(buf.device(), first, last);
+		VUH_CHECKOUT();
 		buf.device().default_transfer().copy_sync(stage, buf);
 	}
 }
 
-
 ///
-template<class BufferView_t, class InputIt, class F>
-auto transform(InputIt first, InputIt last, BufferView_t& buf, F&& f)-> InputIt {
-	throw "not implemented";
+template<class DeviceData_t, class OutputIt, class F>
+auto transform(const DeviceData_t& buf, OutputIt first, F&& f)-> OutputIt {
+	if(buf.host_visible()){
+		auto data = buf.host_data();
+		std::transform(std::begin(data), std::end(data), first, std::forward<F>(f));
+	} else {
+		using Stage = BufferHost< typename DeviceData_t::value_type
+		                        , AllocatorDevice<allocator::traits::HostCached>>;
+		auto stage = Stage(buf.device(), buf.size());
+		VUH_CHECKOUT_RET(first);
+		buf.device().default_transfer().copy_sync(buf, stage);
+		VUH_CHECKOUT_RET(first);
+		std::transform(std::begin(stage), std::end(stage), first, std::forward<F>(f));
+	}
+	return std::next(first, buf.size());
 }
 
 ///
-template<class BufferView_t, class OutputIt, class F>
-auto transform(const BufferView_t& buf, OutputIt first, F&& f)-> OutputIt {
-	throw "not implemented";
+template<class DeviceData_t, class InputIt, class F>
+auto transform(InputIt first, InputIt last, DeviceData_t& buf, F&& f)-> void {
+	assert(std::distance(first, last) <= buf.size());
+	if(buf.host_visible()){
+		auto data = buf.host_data();
+		std::transform(first, last, std::begin(data), std::forward<F>(f));
+	} else {
+		using Stage = BufferHost<typename DeviceData_t::value_type
+		                        , AllocatorDevice<allocator::traits::HostCoherent>>;
+		auto stage = Stage(buf.device(), first, last, std::forward<F>(f));
+		VUH_CHECKOUT();
+		buf.device().default_transfer().copy_sync(stage, buf);
+	}
 }
 
-
 ///
-template<class C, class BufferView_t>
-auto to_host(const BufferView_t& buf)-> traits::Iterable<C> {
-	throw "not implemented";
+template<class C, class DeviceData_t>
+auto to_host(const DeviceData_t& buf)-> traits::Iterable<C> {
+	auto ret = C(buf.size());
+	copy(buf, std::begin(ret));
+	return ret;
 }
 
 ///
-template<class C, class F, class BufferView_t >
-auto to_host(const BufferView_t& data, F&& f)-> traits::Iterable<C> {
-	throw "not implemented";
+template<class C, class F, class DeviceData_t >
+auto to_host(const DeviceData_t& buf, F&& f)-> traits::Iterable<C> {
+	auto ret = C(buf.size());
+	transform(buf, std::begin(ret), std::forward<F>(f));
+	return ret;
 }
 
 } // namespace vuh
--- src/include/vuh/bufferHost.hpp
+++ src/include/vuh/bufferHost.hpp
@@ -9,20 +9,18 @@
 
 namespace vuh {
 
-/// Array with host data exchange interface suitable for memory allocated in host-visible space.
-/// Memory allocation and underlying buffer creation is managed by allocator defined by a template parameter.
-/// Such allocator is expected to allocate memory in host-visible GPU memory, mappable
+/// Buffer with memory allocated in host-visible space.
+/// Allocator template parameter is expected to allocate memory in host-visible GPU memory, mappable
 /// for host access.
-/// Provides Forward iterator + random-access interface.
 /// Memory remains mapped during the whole lifetime of an object of this type.
+/// Provides Forward iterator + random-access interface.
 template<class T, class Alloc>
 class BufferHost: public BufferBase<Alloc>, public HostData<T, BufferBase<Alloc>> {
 	using Base = BufferBase<Alloc>;
 public:
 	using value_type = T;
-	/// Construct object of the class on given device.
-	/// Memory is not initialized with any data.
-	BufferHost(const vuh::Device& device      ///< device to create array on
+	/// Construct buffer of given size. Associated memory is not initialized with any data.
+	BufferHost(const vuh::Device& device      ///< device to create the buffer
 	          , std::size_t n_elements        ///< number of elements
 	          , VkMemoryPropertyFlags flags_memory={} ///< additional (to defined by allocator) memory usage flags
 	          , VkBufferUsageFlags flags_buffer={}    ///< additional (to defined by allocator) buffer usage flags
@@ -31,7 +29,7 @@ public:
 	   , HostData<T, Base>(*this, n_elements)
 	{}
 
-	/// Construct array on given device and initialize with a provided value.
+	/// Construct buffer and initialize with a provided value.
 	BufferHost(const vuh::Device& device ///< device to create array on
 	          , std::size_t n_elements   ///< number of elements
 	          , T value             ///< initializer value
@@ -44,11 +42,10 @@ public:
 		std::fill_n(this->begin(), n_elements, value);
 	}
 
-	/// Construct array on given device and initialize it from range of values
+	/// Construct buffer and initialize it from range of values
 	template<class InputIt>
-	BufferHost(const Device& device      ///< device to create array on
-	          , InputIt first           ///< beginning of initialization range
-	          , InputIt last            ///< end (one past end) of initialization range
+	BufferHost( const Device& device                  ///< device to create the buffer
+	          , InputIt first, InputIt last           ///< host range to use for initialization
 	          , VkMemoryPropertyFlags flags_memory={} ///< additional (to defined by allocator) memory usage flags
 	          , VkBufferUsageFlags flags_buffer={}    ///< additional (to defined by allocator) buffer usage flags
 	          )
@@ -58,7 +55,20 @@ public:
 		std::copy(first, last, this->begin());
 	}
 
-	/// @todo return non-owning host data class by value
+	/// doc me
+	template<class InputIt, class F>
+	BufferHost( const Device& device
+	          , InputIt first, InputIt last
+	          , F&& f
+	          , VkMemoryPropertyFlags flags_memory={} ///< additional (to defined by allocator) memory usage flags
+	          , VkBufferUsageFlags flags_buffer={}    ///< additional (to defined by allocator) buffer usage flags
+	          )
+	   : BufferHost(device, std::distance(first, last), flags_memory, flags_buffer)
+	{
+		VUH_CHECKOUT();
+		std::transform(first, last, this->begin(), std::forward<F>(f));
+	}
+
 	auto host_data()-> HostDataView<T> {return *this;}
 	auto host_data() const-> HostDataView<const T> {return *this;}
 }; // class BufferHost
