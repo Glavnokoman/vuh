50bde17705790c8846fc37aaf62bce71cb1becc5
slava <s.v.savenko@gmail.com>
2019-05-23 23:04:38 +0200

    experimenting with submission interface

--- src/include/vuh/queue.hpp
+++ src/include/vuh/queue.hpp
@@ -3,8 +3,23 @@
 #include <vulkan/vulkan.h>
 
 #include <cstdint>
+#include <vector>
 
 namespace vuh {
+
+///
+class Submission{
+public:
+
+//	auto run()-> Submission;
+//	auto cb()-> Submission; // set compute barrier
+// auto tb()-> Submission; // set transfer barrier
+
+private:
+	VkDevice _device; ///< non-owning
+	VkQueue  _queue;  ///< non-owning
+	std::vector<VkSubmitInfo> _submits;
+}; // class Submission
 
 /// Queue in a context of a compute device
 class Queue {
@@ -25,11 +40,15 @@ public:
 		copy_sync(src.buffer(), dst.buffer(), src.size_bytes(), src.offset_bytes(), dst.offset_bytes());
 	}
 
+	///
+	auto run()-> Submission {
+
+	}
 private:
-	VkQueue _handle;
-	VkDevice _device;
-	VkCommandPool _command_pool;  ///<
-	std::uint32_t _family_id;     ///< family id
+	VkQueue _handle;              ///< owned queue handle
+	VkDevice _device;             ///< non-owned logical device handle containing the queue and other stuff
+	VkCommandPool _command_pool;  ///< non-owned command pool handle. command pool should correspond to the queue family to which the queue handle belongs
+	std::uint32_t _family_id;     ///< queue family id. Needed here?
 }; // struct Queue
 
 } // namespace vuh
