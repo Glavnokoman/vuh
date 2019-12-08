85434934b06f4cfe6c65e0a1a18c7a7a0dd8d291
默默 <wangqiang1588@sina.com>
2019-12-08 15:06:06 +0800

    modify class name

--- src/include/vuh/image.hpp
+++ src/include/vuh/image.hpp
@@ -9,38 +9,38 @@
 namespace vuh {
     namespace detail {
 
-/// Traits to associate array properties to underlying Image classes (DeviceImage,DeviceOnlyImage).
-/// Default mapping is ArrayClass<T> -> arr::HostArray.
+        /// Traits to associate array properties to underlying Image classes (DeviceImage,DeviceOnlyImage).
+        /// Default mapping is Image2DClz<T> -> arr::HostArray.
         template<class Props>
-        struct ImageClass {
+        struct Image2DClz {
             template<class T, class Alloc> using type = vuh::img::DeviceImage2D<T, Alloc>;
         };
 
-/// Explicit trait specialization mapping ImageClass<arr::properties::Device> -> arr::Device2DImage
+        /// Explicit trait specialization mapping Image2DClz<arr::properties::Device> -> arr::Device2DImage
         template<>
-        struct ImageClass<vuh::mem::properties::Device>{
+        struct Image2DClz<vuh::mem::properties::Device>{
             template<class T, class Alloc> using type = vuh::img::DeviceImage2D<T, Alloc>;
         };
 
         template<class Props>
-        struct SamplerClass {
+        struct CombinedImage2DClz {
             template<class T, class Alloc> using type = vuh::img::DeviceCombinedImage2D<T, Alloc>;
         };
 
-/// Explicit trait specialization mapping ImageClass<arr::properties::Device> -> arr::Device2DImage
+        /// Explicit trait specialization mapping CombinedImage2DClz<arr::properties::Device> -> arr::Device2DImage
         template<>
-        struct SamplerClass<vuh::mem::properties::Device>{
+        struct CombinedImage2DClz<vuh::mem::properties::Device>{
             template<class T, class Alloc> using type = vuh::img::DeviceCombinedImage2D<T, Alloc>;
         };
     } // namespace detail
 
-/// Maps Image classes with different data exchange interfaces, to a single template type.
-/// This enables type declarations of Images with different allocators.
-/// Although in this case resulting classes have different data exchange interfaces,
-/// this may still be useful since they all have similar binding properties to shaders.
+    /// Maps Image classes with different data exchange interfaces, to a single template type.
+    /// This enables type declarations of Images with different allocators.
+    /// Although in this case resulting classes have different data exchange interfaces,
+    /// this may still be useful since they all have similar binding properties to shaders.
     template<class T, class Alloc=vuh::mem::AllocDevice<vuh::mem::properties::Device>>
-    using Image2D = typename detail::ImageClass<typename Alloc::properties_t>::template type<T, Alloc>;
+    using Image2D = typename detail::Image2DClz<typename Alloc::properties_t>::template type<T, Alloc>;
 
     template<class T, class Alloc=vuh::mem::AllocDevice<vuh::mem::properties::Device>>
-    using CombinedImage2D = typename detail::SamplerClass<typename Alloc::properties_t>::template type<T, Alloc>;
+    using CombinedImage2D = typename detail::CombinedImage2DClz<typename Alloc::properties_t>::template type<T, Alloc>;
 } // namespace vuh
